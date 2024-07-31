abc63f79b3268fdd4363a4de57c41fa88191211b
Mai2aa <mysaadil0@gmail.com>
2024-07-31 03:05:43 +0200

    Clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,7 @@
 export default function cleanSet(set, startString) {
+  if (typeof startString !== 'string') {
+    throw new Error('Inavalis argumant');
+  }
   const cleanValues = [];
   for (const value of set) {
     if (typeof value === 'string' && value.startsWith(startString)) {
