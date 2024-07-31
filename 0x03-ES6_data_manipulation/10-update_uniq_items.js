0a39932a58b3c4e343e911e144e4beb00832d38c
Mai2aa <mysaadil0@gmail.com>
2024-07-31 03:34:47 +0200

    Update unique items

--- 0x03-ES6_data_manipulation/10-update_uniq_items.js
+++ 0x03-ES6_data_manipulation/10-update_uniq_items.js
@@ -1,6 +1,6 @@
 export default function updateUniqueItems(map) {
   if (!(map instanceof Map)) {
-    throw new Error('Connot process');
+    throw new Error('Cannot process');
   }
   map.forEach((value, key) => {
     if (value === 1) {
