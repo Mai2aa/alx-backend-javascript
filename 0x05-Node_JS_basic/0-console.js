f30b7746f8ba7a62c8facf541095a477dece8a9c
Mai2aa <mysaadil0@gmail.com>
2024-09-19 04:37:52 +0300

    basic javascript with node js

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,8 +1,12 @@
 /**
  * basics of javascript - nodejs
+ * @message - the message to log
+ * @returns - the message itself
  */
 const displayMessage = (message) => console.log(message);
 /**
  * Exports the displayMessage function
+ * @module displayMessage
+ * @function
  */
 module.exports = displayMessage;
